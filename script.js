42f8d5e62d4b787b6d0b11e2b91c3aa476e53c1e
Fer-Win <102341775+Fer-Win@users.noreply.github.com>
2023-04-20 17:45:28 +0000

    Updated the name function

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const randomBtn= document.getElementById("randomHero");
 const imgDiv = document.getElementById("heroImg");
+const heroStats = document.getElementById("heroStats");
 const  heroBtn = document.getElementById("getHero");
 const heroSearch = document.getElementById("searchInput");
-const heroName = document.querySelector(".heroName")
+// const heroName = document.querySelector(".heroName")
 
 
 const access_token = '1384550775447844'
@@ -16,8 +17,11 @@ const getSearchHero =(name)=>{
   .then(json=>{
     console.log(json)
     imgDiv.innerHTML='';
-    heroName.innerText = json.results[0].name;
-     imgDiv.innerHTML +=`<img height=300 width=300 src="${json.results[0].image.url}">`
+     const name = `<h2>${json.results[0].name}</h2>`
+    // heroName.innerText = json.results[0].name;
+     imgDiv.innerHTML =`${name}<img height=300 width=300 src="${json.results[0].image.url}">`
+    heroStats.innerHTML = JSON.stringify(json.results[0].powerstats)
+    
   // json.results.forEach(result =>{
   //       heroName.innerText = result.name;
   //         imgDiv.innerHTML +=`<img height=300 width=300 src="${result.image.url}">`
@@ -28,9 +32,10 @@ const getRandomSuperHero= (id) =>{
   .then(response=>response.json())
   .then(json=>{
     console.log(json)
-     heroName.innerText = json.name;
+     // heroName.innerText = json.name;
     imgDiv.innerHTML='';
-    imgDiv.innerHTML =`<img height=300 width=300 src="${json.image.url}">`
+    const name = `<h2>${json.name}</h2>`
+    imgDiv.innerHTML =`${name}<img height=300 width=300 src="${json.image.url}">`
   })
 }
 
